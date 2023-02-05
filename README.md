ebcef123867eb0323b5308b21b17d682e1bbd0e4
Athanasios Patralis <sakispatralis@gmail.com>
2023-02-06 00:24:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python Project
+This is a the in python project and create in the bash script.
+
+```bash
+    ./python-project.sh
+```
---
+++ python-project.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# Create folder
+echo "Creating folder"
+mkdir src
+cd src
+
+# Create file
+echo "Creating file"
+touch main.py
+
+# Open text editor
+nano main.py
+
+# Run project
+python main.py
